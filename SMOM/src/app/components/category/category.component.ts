5c966a88ea2d3f59c0299f3c6fdbed354d6ad907
Chetan C <chetan41082@gmail.com>
2022-06-01 08:58:01 +0530

    Added Cateogory Component

--- SMOM/src/app/components/category/category.component.ts
+++ SMOM/src/app/components/category/category.component.ts
@@ -9,7 +9,41 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   constructor() { }
+  categoryDetails: Array<any>=[
+    {
+        id:1,
+        name: "Automobile",
+        description: '<b> Automobile Cars/bike<b>'
 
+    } ,
+    {
+       id: 2,
+       name: "Electronics",
+       description: '<b> Electrnic TV/Mobile<b>'
+    },
+    {
+      id: 3,
+      name: "Electrical",
+      description: '<b> Eletrical Cars/bike<b>'
+   },
+   {
+    id: 4,
+    name: "Manual",
+    description: '<b> Manula Cars/bike<b>'
+   },
+
+  ];
+
+  orderBy = -1;
+
+  sortColumn(columnName:any) {
+    this.orderBy = this.orderBy*-1
+    let orderBy = this.orderBy;
+    this.categoryDetails.sort(function(a,b){
+      return a[columnName]>b[columnName]? -1*orderBy : 1*orderBy;
+    })
+  }
+  
   ngOnInit(): void {
   }
 
